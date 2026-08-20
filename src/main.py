9ea84a282636d57914b4e7589e98aaafb8b52b3e
Jai Preet Singh <233284235+jpsinghKG@users.noreply.github.com>
2026-08-20 11:27:27 -0400

    feat: :sparkles: persist linear records to db

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 
 from collectors.git_local_collector.git_local_collector import collect as collect_git_local_events
 from collectors.github_collector.github_collector import collect as collect_github_events
+from collectors.linear_collector.linear_collector import collect as collect_linear_events
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from pathlib import Path
@@ -13,12 +14,14 @@ from utils.collectors import parse_identities, parse_roots
 
 load_dotenv()
 
+now = datetime.now(UTC)
+since = now - timedelta(days=1)
+until = now + timedelta(days=1)
+
 
 def get_git_local_events() -> list[Event]:
     roots = parse_roots(os.getenv("GIT_LOCAL_ROOTS"))
     identities = parse_identities(os.getenv("GIT_LOCAL_IDENTITIES"))
-    since = datetime.today() - timedelta(days=1)
-    until = datetime.today() + timedelta(days=1)
     include_patch = False
 
     return collect_git_local_events(roots, identities, since, until, include_patch)
@@ -26,9 +29,6 @@ def get_git_local_events() -> list[Event]:
 
 def get_github_events() -> list[Event]:
     identities = parse_identities(os.getenv("GITHUB_IDENTITIES"))
-    now = datetime.now(UTC)
-    since = now - timedelta(days=1)
-    until = now + timedelta(days=1)
     include_patch = False
 
     return collect_github_events(identities, since, until, include_patch)
@@ -37,8 +37,13 @@ def get_github_events() -> list[Event]:
 if __name__ == "__main__":
     git_local_events = get_git_local_events()
     github_events = get_github_events()
+    linear_events = collect_linear_events(since, until)
 
-    events = [*git_local_events, *github_events]
+    events = [
+        *git_local_events, 
+        *github_events, 
+        *linear_events
+    ]
 
     conn = connect(Path("db/bragbook.db"))
     upsert_events(conn, events)
